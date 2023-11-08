596c74725a5f3e1a7167759c20a2e8f46bf14abb
le-koj <mitchkojelegance@gmail.com>
2023-11-08 21:33:59 +0000

    doc: improved basic docstring to numpy-style with examples for functions - filter_unique_urls, get_pages_from_sitemap in links_check.py

--- links_check.py
+++ links_check.py
@@ -4,9 +4,6 @@ This script allows the user to automate the inspection of all
 the internal and external links on a website. The script allows
 the user to audit a website for broken links.
 
-This script requires the senders email address, email password,
-the receiver's email, and the email subject.
-
 This script uses the following libraries:
     ultimate sitemap parser
     requests library
@@ -27,17 +24,44 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from typing import Tuple, List
+import settings
 
 DOMAIN_NAME = "" 
 
 def get_pages_from_sitemap(domain_url: str) -> list:
-    """Returns a list of all the webpages on a website.
+    """
+    Retrieves a list of URLs from the sitemap of the provided domain.
 
-    Args:
-        domain_url (str): A string representing the website home address, ex. 'https://example.com/'.
+    Parameters
+    ----------
+    domain_url : str
+        The URL of the domain for which the sitemap is to be fetched.
 
-    Returns:
-        list: returns a raw list of webpage urls.
+    Returns
+    -------
+    list
+        A list of URLs extracted from the sitemap.
+
+    Notes
+    -----
+    This function depends on the 'sitemap_tree_for_homepage' function, which is assumed to
+    return an iterator object representing the pages in the sitemap.
+
+    Examples
+    --------
+    >>> domain_url = "https://example.com"
+    >>> pages = get_pages_from_sitemap(domain_url)
+    >>> len(pages) > 0
+    True
+
+    >>> all(isinstance(url, str) for url in pages)
+    True
+
+    >>> 'https://example.com/page1' in pages
+    True
+
+    >>> 'https://example.com/nonexistent' in pages
+    False
     """
     
     sitemap_pages = sitemap_tree_for_homepage(domain_url).all_pages() # returns iterator object
@@ -45,13 +69,39 @@ def get_pages_from_sitemap(domain_url: str) -> list:
     return url_raw_list
 
 def filter_unique_urls(domain_url: str) -> set:
-    """Return a set of unique urls from list of raw urls.
+    """
+    Filter out duplicate URLs from the sitemap of the provided domain.
 
-    Args:
-        domain_url (str): A string representing the website address, ex. 'https://example.com/'.
+    Parameters
+    ----------
+    domain_url : str
+        The URL of the domain for which the sitemap is to be fetched.
 
-    Returns:
-        set: returns a unique set of urls.
+    Returns
+    -------
+    set
+        A set of unique URLs extracted from the sitemap.
+
+    Notes
+    -----
+    This function relies on the 'get_pages_from_sitemap' function, which retrieves a list
+    of URLs from the sitemap and removes duplicates by converting them to a set.
+
+    Examples
+    --------
+    >>> domain_url = "https://example.com"
+    >>> unique_urls = filter_unique_urls(domain_url)
+    >>> len(unique_urls) > 0
+    True
+
+    >>> isinstance(unique_urls, set)
+    True
+
+    >>> 'https://example.com/page1' in unique_urls
+    True
+
+    >>> 'https://example.com/page1' in filter_unique_urls(domain_url + '/duplicate-sitemap/')
+    False
     """
     
     # filter out duplicate urls
@@ -77,7 +127,6 @@ def link_list(list_pages: list) -> Tuple[List[list], List[list]]:
     print(f"\n-----\nDomain URL: {DOMAIN_NAME}\n-----\n")
             
     def _create_link_lists(list_of_links):
-        #print(f"\n-----\n{list_of_links}\n-----\n")
         for link in list_of_links:
             try:
                 if DOMAIN_NAME in link["href"] and "http" in link["href"]:
@@ -91,7 +140,7 @@ def link_list(list_pages: list) -> Tuple[List[list], List[list]]:
     
     for url in list_pages:
         count = count + 1
-        request = requests.get(url, headers=USER_AGENT)
+        request = requests.get(url, headers=settings.USER_AGENT)
         content = request.content
         soup = BeautifulSoup(content, 'lxml')
         list_of_links = soup.find_all("a")
